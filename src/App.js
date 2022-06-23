bdf108ddb51b8ad2a788b46d6378ad7b72d71d47
onur <onureredo@yahoo.com>
2022-06-23 18:19:01 +0200

    added new stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css';
+// import './main.js';
+import profilePic from './images/profilePic.jpeg';
+import logomain from './images/logomain.png';
 
-function App() {
+export default () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="EASY">
+    <body>    
+    <header>        
+        <div class="header">
+            <div class="profile">                
+                <img id="prof-pic" src={profilePic}/>
+                <h3>Hello, Onur!</h3>
+            </div>
+        </div>          
+    </header>
+    <main>        
+      <section id="contentpaper">        
+            <img id="logomain" src={logomain}/>
+            <h1>To Do List!</h1>            
+        <form id="new-task-form">
+            <input type="text" id="new-task-input" placeholder="What's your plan for today?"/>
+            <button type="submit" id="new-task-submit"></button>
+        </form>
+        <div id="task-list">
+            <h2>Tasks</h2>
+            <hr/>
+            <div id="tasks">                    
+            </div>
+        </div>
+      </section>
+    </main> 
+    <script src="main.js"></script>   
+    </body>
+            
+</div> 
   );
-}
-
-export default App;
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '../style.css';
+// import '../main.js';
+import profilePic from '../images/profilePic.jpeg';
+import logomain from '../images/logomain.png';
+
 
 test('renders learn react link', () => {
   render(<App />);
