0d6bd12de8620cbcd842c878913c666b981f296d
chinch42 <vanessakolund@gmail.com>
2022-06-24 09:10:20 +0200

    Update Local Storage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, { useState } from 'react'
-import './App.css';
-import Form from './components/Form'
-import TodoList from './components/TodoList';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  //useffect for lcoal storage
+  useEffect((todos) => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todolocal = JSON.parse(localStorage.getItem("todos"));
+      //set current state to local todo
+      setTodos(todolocal);
+      console.log(todolocal);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="App">
-      <header>        
+      <header>
         <div className="header">
-            <div className="profile">                
-                <h3>Hello, user!</h3>
-            </div>
-        </div>          
+          <div className="profile">
+            <h3>Hello, user!</h3>
+          </div>
+        </div>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
